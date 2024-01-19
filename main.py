3f2f25dc8f22e20ee41c9a0e3543b0a103b0fea7
natya <natyavidhanbiswas10@gmail.com>
2024-01-19 13:03:26 +0530

    loading cookies

--- main.py
+++ main.py
@@ -2,18 +2,9 @@ import pyttsx3
 engine = pyttsx3.init()
 from bardapi import BardCookies
 import speech_recognition as sr
+import json
 
-print("start")
-print()
-
-
-cookie_dict = {
-    "__Secure-1PSID": "egjP4T18AjffIk2CBL3i_Rf_XWHdyyRfM9uonNn-ih7HsSGeUKhIAKNEyrHqdEulqxdJNQ.",
-    "__Secure-1PSIDTS": "sidts-CjIBPVxjSgE0nPdG9ALtEvWPNTrPdTTsjGBjdli-lnkJgbjPt7e77Cnk037KFByBkA2HixAA",
-    "__Secure-1PSIDCC": "ABTWhQGnXcnotJkWb9jfomtkeOfqJuf7QQLMMz3XlVPckv_uQit3g7rBQFajArAAqKQWhPRZMw",
-    "__Secure-1PAPISID": "10DU3mWwvXrka-zg/A9xlK92-wiDEFzGzT",
-    # Any cookie values you want to pass session object.
-}
+cookie_dict = {i["name"]: i['value'] for i in json.load(open("cookies.json"))}
 
 bard = BardCookies(cookie_dict=cookie_dict)
 voices = engine.getProperty('voices')
@@ -25,14 +16,15 @@ while True:
     with sr.Microphone() as source:
         print("Say something!")
         audio = r.listen(source, phrase_time_limit=5)
-
+    
     print("transcribing...")
-
     query = r.recognize_google(audio)
+
+    print(f"Your query: {query}")
 
     print("processng...")
 
-    ans = bard.get_answer(query)['content']
+    ans = bard.get_answer(query + ". give a short answer")['content']
 
     engine.say(ans)
     engine.runAndWait()
