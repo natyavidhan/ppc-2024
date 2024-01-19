f2b1e526a97de694d462554fec972cd0b0c2020b
natya <natyavidhanbiswas10@gmail.com>
2024-01-19 13:12:50 +0530

    cleaning

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import pyttsx3
-engine = pyttsx3.init()
 from bardapi import BardCookies
 import speech_recognition as sr
 import json
 
 cookie_dict = {i["name"]: i['value'] for i in json.load(open("cookies.json"))}
-
 bard = BardCookies(cookie_dict=cookie_dict)
+
+engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 engine.setProperty('volume', 1.0)
 
+r = sr.Recognizer()
+
 while True:
-    r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Say something!")
         audio = r.listen(source, phrase_time_limit=5)
